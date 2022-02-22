92aa6a9d0ad400b0f5e26cdfc4ee82a03df5d18a
John Paxton (TJX) <tjx@speedingplanet.com>
2022-02-22 12:13:20 -0500

    Add code to demo ESLint

--- js/hello-world.js
+++ js/hello-world.js
@@ -1,1 +1,7 @@
-console.log("Hello, world, from a script src tag");
+console.log('Hello, world, from a script src tag');
+
+let myName = 'John';
+console.log(`My name is ${myName}`);
+
+const amount = 10;
+console.log(`Some amount: ${amount}`);
