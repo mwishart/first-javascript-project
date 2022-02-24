1aed27311c2bc70ba29c6350d9a6f4649c2e467b
John Paxton <johnpaxton@gmail.com>
2022-02-24 10:06:03 -0500

    Add explanatory comments

--- js/form-data.js
+++ js/form-data.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#movie-form');
 form.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
+  // DO NOT actually submit the form
   event.preventDefault();
   let movieData = new FormData(form);
 
@@ -22,6 +23,7 @@ function iterateWithKeys(movieData) {
 
 function iterateWithEntries(movieData) {
   console.log('Iterating with entries:');
+  // [['title', 'Encanto'], ['year', '2022'], ['genre', 'drama']]
   for (let entry of movieData.entries()) {
     console.log(`${entry[0]}: ${entry[1]}`);
   }
@@ -29,6 +31,9 @@ function iterateWithEntries(movieData) {
 
 function iterateWithEntriesDestructured(movieData) {
   console.log('Iterating with entries (destructured):');
+  // let entry = movieData.nextEntry()
+  // let key = entry[0]
+  // let value = entry[1]
   for (let [key, value] of movieData.entries()) {
     console.log(`${key}: ${value}`);
   }
